79e0bd26b6799e005b2f012bcca98496d70d6714
tobiasjohansson89 <35295187+tobiasjohansson89@users.noreply.github.com>
2023-06-15 02:13:30 +0200

    changed color theme

--- js/water.mjs
+++ js/water.mjs
@@ -1,7 +1,29 @@
-export default function waterToogler() {
+// export default function waterToogler() {
+//     const btn = document.getElementById("toogle-water")
+//     console.log("hello from waterToogler")
+//     btn.addEventListener("click", () => {
+//         console.log(btn.checked)
+//     })
+// }
+
+
+export function outer(){
+    const message = document.getElementById("water-text")
     const btn = document.getElementById("toogle-water")
-    console.log("hello from waterToogler")
+
     btn.addEventListener("click", () => {
-        console.log(btn.checked)
+
+        if(btn.checked === true){
+            message.innerHTML = "ON"
+            //return console.log("return")
+        }
+        else {
+            message.innerHTML = "OFF"
+        }
+        //console.log(btn.checked)
+        return btn.checked
     })
+
 }
+
+export default outer
