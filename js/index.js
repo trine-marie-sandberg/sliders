b6b3f1c139587d820890b8cd00eb6dbd2b481a0f
tobiasjohansson89 <35295187+tobiasjohansson89@users.noreply.github.com>
2023-06-15 02:13:33 +0200

    color theme

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
-import waterToogler from "./water.mjs"
-waterToogler();
+import {outer, test} from "./water.mjs"
+
+const valuecheck = outer();
+let isChecked = false
+console.log(valuecheck)
+let isresults = test(isChecked)
+console.log(`logging my test function: ${isresults}`)
